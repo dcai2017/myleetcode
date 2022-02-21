c92903d44cbadd19c90712606841ff301e04a421
dcai2017 <corndogfor2003@yahoo.com>
2022-02-20 22:33:07 -0600

    Added UnionFind template

--- Templates/UnionFind/src/myleetcode/templates/UnionFind.java
+++ Templates/UnionFind/src/myleetcode/templates/UnionFind.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class UnionFind {
+package myleetcode.templates;
+
+public class UnionFind {
+    int [] father;
+    public UnionFind(int N) {
+        father = new int[N];
+    }
+    int findFather(int x) {
+        if (father[x]!=x)
+            father[x]=findFather(father[x]);
+        return father[x];
+    }
+    void union(int x, int y) {
+        int px=findFather(x);
+        int py=findFather(y);
+        if (px!=py)
+            father[py]=px;
+    }
 }
